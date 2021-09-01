8ebbd85810e9e09e966c16fd03b67ad2d55f1d65
neoterux <labfernandez2014@gmail.com>
2021-08-31 23:34:56 -0500

    First implementation of keymap

---
+++ keyboards/crkbd/keymaps/neoterux/enums.h
@@ -0,0 +1,15 @@
+
+// Layers
+typedef enum {
+    _QWERTY,
+    _LEVEL2,
+    _LEVEL3,
+    _DVORAK
+} CRKBD_LAYERS;
+
+// Tap dance keys
+// enum {
+//     TD_CLOCK,
+// }
+
+extern enum CRKBD_LAYERS crkbd_layers;
