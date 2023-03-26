982c1fa67187bd0b2a6f390431073f042defaef2
Neoterux <labfernandez2014@gmail.com>
2023-03-26 11:59:42 -0500

    Added semicolon

--- keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
+++ keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_RAISE] = LAYOUT(
                         KC_TILD,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPLY,  _______,
     _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_HOME,  KC_END,   KC_QUOT,  KC_UP,    XXXXXXX,  XXXXXXX,  KC_DQUO,
+    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_HOME,  KC_END,   KC_QUOT,  KC_UP,    KC_SCLN,  XXXXXXX,  KC_DQUO,
     _______,  XXXXXXX,  KC_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RIGHT,           KC_QUES,
     _______,  _______,  _______,               _______,                 _______,              KC_LALT,  _______,  _______
 	),
