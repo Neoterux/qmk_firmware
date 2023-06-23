58d65d9e72f764248ab72fdba5af9d9e086d34d9
Luis Bajaña <labfernandez2014@gmail.com>
2023-06-23 09:18:43 -0500

    Added Grave key instead of tilde

--- keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
+++ keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,   KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,             KC_SLSH,
-    KC_LCTL,  KC_LGUI,  LOWER,              KC_SPC,                   SP_RA,                  KC_RALT,  LOWER,    KC_SYRQ
+    KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                   SP_RA,                  KC_RALT,  LOWER,    KC_SYRQ
 	),
 	[_LOWER] = LAYOUT(
                         QK_GESC,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_PAUS,  KC_SCRL,  KC_PSCR,  _______,
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,               _______,                 _______,              KC_LALT,  _______,  _______
 	),
 	[_RAISE] = LAYOUT(
-                        KC_TILD,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPLY,  _______,
+                        KC_GRAVE, KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPLY,  _______,
     _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
     _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_HOME,  KC_END,   KC_QUOT,  KC_UP,    KC_SCLN,  XXXXXXX,  KC_DQUO,
     _______,  XXXXXXX,  KC_BSLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT,  KC_DOWN,  KC_RIGHT,           KC_QUES,
