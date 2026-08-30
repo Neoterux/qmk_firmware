e5e7cbabd28fee3c82a4fcd99cf095a9f197ffce
Neoterux <labfernandez2014@gmail.com>
2026-08-30 14:19:30 -0500

    Make Caps Lock a tap/hold key for the LOWER layer
    Caps Lock (home row, left pinky) is rarely held, unlike RALT which is
    used for AltGr punctuation and needs to stay put. Turn it into
    LT(_LOWER, KC_CAPS): tap still toggles Caps Lock, hold activates
    LOWER without the pinky stretch to the old bottom-row LOWER key
    (which remains as a backup).

--- keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
+++ keyboards/reviung/reviung53/keymaps/neoterux/keymap.c
@@ -15,7 +15,8 @@ enum layer_names {
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 
-#define SP_RA LT(RAISE, KC_SPC)
+#define SP_RA   LT(RAISE, KC_SPC)
+#define CAPS_LO LT(_LOWER, KC_CAPS)
 
 // Hyprland quick actions (same bindings as the crkbd keymap)
 #define HYPR_TERM LWIN(KC_Q)  // Open terminal
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT(
                         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_DEL,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,   KC_ENT,
+    CAPS_LO,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,   KC_ENT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,             KC_SLSH,
     KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                   SP_RA,                  KC_RALT,  LOWER,    KC_SYRQ
 	),
